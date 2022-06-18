82bd46e52e997f0bd62714500a7863f28b220f5a
helder-hanka <hfernandes238@gmail.com>
2022-06-19 00:53:40 +0200

    Add middleware controller's errors

--- index.js
+++ index.js
@@ -36,6 +36,14 @@ app.use((req, res, next) => {
 
 require("./routes")(app);
 
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
 mongoose
   .connect(`mongodb://${DB_HOST}:${DB_PORT}/CVshop`)
   .then((server) => {
