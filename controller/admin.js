64ece57f398e81c484e825f9b0d8fa420a8e8930
helder-hanka <hfernandes238@gmail.com>
2022-06-19 05:10:46 +0200

    delete validationResult's getProductsAdminIdAll

--- controller/admin.js
+++ controller/admin.js
@@ -58,11 +58,6 @@ const createProduct = async (req, res, next) => {
 };
 
 const getProductsAdminIdAll = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
-  }
-
   const admin = admin3._id;
 
   try {
