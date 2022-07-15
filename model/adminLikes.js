eeb9d455e92b72fdcbe83e2fd7bfc50f71bfc0d0
helder-hanka <hfernandes238@gmail.com>
2022-07-15 12:04:30 +0200

    Add timestamps

--- model/adminLikes.js
+++ model/adminLikes.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const likesSchema = new Schema({
-  likes: {
-    type: Number,
-  },
-  admin: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "Admin",
-  },
-  users: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
+const likesSchema = new Schema(
+  {
+    likes: {
+      type: Number,
     },
-  ],
-});
+    admin: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Admin",
+    },
+    users: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Like", likesSchema);
