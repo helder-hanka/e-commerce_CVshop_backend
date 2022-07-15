e90965fea72b14c852581e6f7dc257b5a30f5ccc
helder-hanka <hfernandes238@gmail.com>
2022-07-15 12:09:59 +0200

    not change

---
+++ routes/auths/adminCvShop.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+
+const { body } = require("express-validator");
+const createAdminCvShopController = require("../../controller/auths/adminCvShop");
+const upload = require("../../middlewares/multerAdminCvShop");
+const AdminCvShop = require("../../model/adminCvShop");
+const clearImg = require("../../lib/clearImg");
+
+router.post(
+  "/admin_cv_shop/signup",
+  upload.single("image"),
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .normalizeEmail({ gmail_remove_dots: false })
+      .custom(async (value, { req }) => {
+        const image = req.file.path;
+        const user = await AdminCvShop.findOne({ email: value });
+        if (user) {
+          clearImg(image);
+          return Promise.reject("E-mail already exist");
+        }
+      }),
+    body("password").trim().not().isEmpty(),
+  ],
+
+  createAdminCvShopController.createAdminCvShop
+);
+
+router.post(
+  "/admin_cv_shop/login",
+  createAdminCvShopController.loginAdminCvShop
+);
+
+module.exports = router;
