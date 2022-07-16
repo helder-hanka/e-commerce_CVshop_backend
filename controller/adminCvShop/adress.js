e504f6e889693fe94dd6d6771344a7d521a6276b
helder-hanka <hfernandes238@gmail.com>
2022-07-16 23:28:48 +0200

    Add updateAdress

--- controller/adminCvShop/adress.js
+++ controller/adminCvShop/adress.js
@@ -81,7 +81,69 @@ const getAdressById = async (req, res, next) => {
   }
 };
 
+const updatedAdress = async (req, res, next) => {
+  const id = req.params.id;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    clearImg(req.file.path);
+    return res.status(400).json({ message: errors.array() });
+  }
+  const {
+    firstname,
+    lastname,
+    gender,
+    address_line_1,
+    address_line_2,
+    city,
+    postal_code,
+    country,
+    telephone,
+    mobile,
+  } = req.body;
+  let imageUrl = req.body.image;
+
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+
+  if (!imageUrl) {
+    return res.status(422).json({ message: "No file picked" });
+  }
+
+  try {
+    const adress = await Adress.findOne({ admin_cvShop: id });
+    if (!adress) {
+      return res.status(404).json({ message: "Could not find" });
+    }
+    if (imageUrl !== adress.imageUrl) {
+      clearImg(adress.imageUrl);
+    }
+
+    adress.firstname = firstname;
+    adress.lastname = lastname;
+    adress.gender = gender;
+    adress.imageUrl = imageUrl;
+    adress.address_line_1 = address_line_1;
+    adress.address_line_2 = address_line_2;
+    adress.city = city;
+    adress.postal_code = postal_code;
+    adress.country = country;
+    adress.telephone = telephone;
+    adress.mobile = mobile;
+
+    const result = await adress.save();
+    res.status(200).json({ message: "Adress updated!", adress: result });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
 module.exports = {
   createAdress,
   getAdressById,
+  updatedAdress,
 };
--- routes/adminCvShop.js
+++ routes/adminCvShop.js
@@ -3,22 +3,71 @@ const { body } = require("express-validator");
 const adress = require("../controller/adminCvShop/adress");
 const upload = require("../middlewares/multerAdminCvShopAdress");
 const isAuth = require("../middlewares/auths/is-authAdminCvShop");
+const clearImage = require("../lib/clearImg");
 
 router.post(
   "/adress",
   isAuth,
   upload.single("image"),
   [
-    [
-      body("firstname", "Please enter a text end least 3 characters")
-        .trim()
-        .isString()
-        .isLength({ min: 3 }),
-      body("lastname", "Please enter a text end least 3 characters")
-        .trim()
-        .isLength({ min: 3 })
-        .isString(),
-      body("gender").custom((value, { req }) => {
+    body("firstname", "Please enter a text end least 3 characters")
+      .trim()
+      .isString()
+      .isLength({ min: 3 }),
+    body("lastname", "Please enter a text end least 3 characters")
+      .trim()
+      .isLength({ min: 3 })
+      .isString(),
+    body("gender").custom((value, { req }) => {
+      const gender = ["male", "female"];
+      const isValid = gender.some(
+        (arr) => arr === req.body.gender.toLowerCase()
+      );
+      if (!isValid) {
+        throw new Error(`Please use this gender: ${gender}`);
+      }
+      return true;
+    }),
+    body("address_line_1", "Please enter a text end least 3 characters")
+      .trim()
+      .isLength({ max: 50 })
+      .isString(),
+    body("address_line_2", "Please enter a text end least 3 characters")
+      .trim()
+      .isLength({ max: 50 })
+      .isString(),
+    body("city", "Please enter a text end least 3 characters")
+      .trim()
+      .isLength({ min: 3 })
+      .isString(),
+    body("postal_code", "Please enter a number").trim().isInt(),
+    body("country", "Please enter a text end least 3 characters")
+      .trim()
+      .isLength({ min: 3 })
+      .isString(),
+    body("telephone", "Please enter a number").trim().isInt(),
+    body("mobile", "Please enter a number").trim().isInt(),
+  ],
+  adress.createAdress
+);
+
+router.get("/adress/:id", adress.getAdressById);
+router.put(
+  "/adress/:id",
+  upload.single("image"),
+
+  [
+    body("firstname", "Please enter a text end least 3 characters")
+      .trim()
+      .isString()
+      .isLength({ min: 3 }),
+    body("lastname", "Please enter a text end least 3 characters")
+      .trim()
+      .isLength({ min: 3 })
+      .isString(),
+    body("gender")
+      .trim()
+      .custom((value, { req }) => {
         const gender = ["male", "female"];
         const isValid = gender.some(
           (arr) => arr === req.body.gender.toLowerCase()
@@ -28,30 +77,27 @@ router.post(
         }
         return true;
       }),
-      body("address_line_1", "Please enter a text end least 3 characters")
-        .trim()
-        .isLength({ max: 50 })
-        .isString(),
-      body("address_line_2", "Please enter a text end least 3 characters")
-        .trim()
-        .isLength({ max: 50 })
-        .isString(),
-      body("city", "Please enter a text end least 3 characters")
-        .trim()
-        .isLength({ min: 3 })
-        .isString(),
-      body("postal_code", "Please enter a number").trim().isInt(),
-      body("country", "Please enter a text end least 3 characters")
-        .trim()
-        .isLength({ min: 3 })
-        .isString(),
-      body("telephone", "Please enter a number").trim().isInt(),
-      body("mobile", "Please enter a number").trim().isInt(),
-    ],
+    body("address_line_1", "Please enter a text end least 3 characters")
+      .trim()
+      .isLength({ max: 50 })
+      .isString(),
+    body("address_line_2", "Please enter a text end least 3 characters")
+      .trim()
+      .isLength({ max: 50 })
+      .isString(),
+    body("city", "Please enter a text end least 3 characters")
+      .trim()
+      .isLength({ min: 3 })
+      .isString(),
+    body("postal_code", "Please enter a number").trim().isInt(),
+    body("country", "Please enter a text end least 3 characters")
+      .trim()
+      .isLength({ min: 3 })
+      .isString(),
+    body("telephone", "Please enter a number").trim().isInt(),
+    body("mobile", "Please enter a number").trim().isInt(),
   ],
-  adress.createAdress
+  adress.updatedAdress
 );
-
-router.get("/adress/:id", adress.getAdressById);
 
 module.exports = router;
