d659f0a87ba2573239912476c6f50bf79359371c
helder-hanka <hfernandes238@gmail.com>
2022-07-17 04:22:39 +0200

    Add getPayments and updated condition if createPayment

--- controller/adminCvShop/payments.js
+++ controller/adminCvShop/payments.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator");
 const Payment = require("../../model/adminCvShop/payments");
+const AdminCvShop = require("../../model/adminCvShop");
 const addMonths = require("../../lib/addMonths");
 
 const createPayment = async (req, res, next) => {
@@ -7,7 +8,7 @@ const createPayment = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+    return res.status(400).json({ message: errors.array() });
   }
   const body = req.body;
   const payment = new Payment({
@@ -36,6 +37,25 @@ const createPayment = async (req, res, next) => {
   }
 };
 
+const getPayments = async (req, res, next) => {
+  const userId = req.userId;
+  try {
+    const result = await AdminCvShop.findById(
+      userId,
+      "email image adminCvShop createdAt updatedAt"
+    )
+      .populate("Payments")
+      .populate("Adress");
+    res.status(200).json({ message: "Fetched", result: result });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
 module.exports = {
   createPayment,
+  getPayments,
 };
--- routes/adminCvShop.js
+++ routes/adminCvShop.js
@@ -3,6 +3,7 @@ const { body } = require("express-validator");
 const adress = require("../controller/adminCvShop/adress");
 const upload = require("../middlewares/multerAdminCvShopAdress");
 const isAuth = require("../middlewares/auths/is-authAdminCvShop");
+const payment = require("../controller/adminCvShop/payments");
 
 router.post(
   "/adress",
@@ -101,5 +102,34 @@ router.put(
   ],
   adress.updatedAdress
 );
+
+router.post(
+  "/payment",
+  isAuth,
+  [
+    body("payment_type")
+      .trim()
+      .isString()
+      .custom((value, { req }) => {
+        const paymentType = ["cash payment", "online payment"];
+        const isValid = paymentType.some(
+          (arr) => arr === req.body.payment_type.toLowerCase()
+        );
+        if (!isValid) {
+          throw new Error(`Please use this type: ${paymentType}`);
+        }
+        return true;
+      }),
+    body("provider").trim().isString().isLength({ min: 3 }),
+    body("account_no").trim().isInt(),
+    body("permissions").trim().isBoolean(),
+    body("comments").trim().isString().isLength({ min: 5 }),
+    body("validatePaymentReceved").trim().isBoolean(),
+    body("confirmPaymentGive").trim().isBoolean(),
+  ],
+  payment.createPayment
+);
+
+router.get("/payment", isAuth, payment.getPayments);
 
 module.exports = router;
