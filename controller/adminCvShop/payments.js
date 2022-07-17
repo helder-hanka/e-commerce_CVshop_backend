b79d26948ccb0bf61c020c257f8c15d1dab0c83f
helder-hanka <hfernandes238@gmail.com>
2022-07-17 02:08:15 +0200

    deleted clg

---
+++ controller/adminCvShop/payments.js
@@ -0,0 +1,41 @@
+const { validationResult } = require("express-validator");
+const Payment = require("../../model/adminCvShop/payments");
+const addMonths = require("../../lib/addMonths");
+
+const createPayment = async (req, res, next) => {
+  const userId = req.userId;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+  const body = req.body;
+  const payment = new Payment({
+    payment_type: body.payment_type.toLowerCase(),
+    provider: body.provider.toLowerCase(),
+    account_no: body.account_no,
+    expiry: addMonths(1),
+    permissions: body.permissions,
+    paymentDate: new Date(),
+    comments: body.comments.toLowerCase(),
+    validatePaymentReceved: body.validatePaymentReceved,
+    confirmPaymentGive: body.confirmPaymentGive,
+    admin_cvShop: userId,
+  });
+
+  try {
+    const result = await payment.save();
+    res
+      .status(200)
+      .json({ message: "Payment create successfully", payment: result });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+module.exports = {
+  createPayment,
+};
