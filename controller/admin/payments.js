2aef50f098d6a381ccb45daf4a90293f1d2e48da
helder-hanka <hfernandes238@gmail.com>
2022-07-21 02:18:25 +0200

    Delete post and update get

--- controller/admin/payments.js
+++ controller/admin/payments.js
@@ -1,35 +1,50 @@
-const { validationResult } = require("express-validator");
-const Payment = require("../../model/admin/payments");
-const addMonths = require("../../lib/addMonths");
+const Admin = require("../../model/admin/admin");
 
-const createPayment = async (req, res, next) => {
-  const userId = req.userId;
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ message: errors.array() });
+const getPaymentsAdminAllById = async (req, res, next) => {
+  const id = req.userId;
+  try {
+    const payments = await Admin.findById(
+      id,
+      "email image admin_cvShop createdAt updatedAt"
+    )
+      .populate("Payments")
+      .populate("adress");
+    if (!payments) {
+      return res.status(404).json({ message: "Could not find payments" });
+    }
+    if (!payments.Payments.length) {
+      return res.status(404).json({ message: "Could not find payments" });
+    }
+    res.status(200).json({ message: "Fetched", payments: payments });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
   }
-  const body = req.body;
-  const payment = new Payment({
-    amount: body.amount,
-    payment_type: body.payment_type.toLowerCase(),
-    provider: body.provider.toLowerCase(),
-    account_no: body.account_no,
-    expiry: addMonths(1),
-    permissions: body.permissions,
-    paymentDate: new Date(),
-    comments: body.comments.toLowerCase(),
-    validatePaymentReceved: body.validatePaymentReceved,
-    confirmPaymentGive: body.confirmPaymentGive,
-    admin_cvShop: userId,
-    admin: body.admin,
-  });
+};
+
+const getPaymentById = async (req, res, next) => {
+  const id = req.params.id;
+  const userId = req.userId;
 
   try {
-    const result = await payment.save();
-    res
-      .status(200)
-      .json({ message: "Payment create successfully", payment: result });
+    const payments = await Admin.findById(
+      userId,
+      "email image adminCvShop createdAt updatedAt"
+    )
+      .populate({
+        path: "Payments",
+        match: { _id: id },
+      })
+      .populate("Adress");
+    if (!payments) {
+      return res.status(404).json({ message: "Could not find payments" });
+    }
+    if (!payments.Payments.length) {
+      return res.status(404).json({ message: "Could not find payments" });
+    }
+    res.status(200).json({ message: "Fetched", payments: payments });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
@@ -39,5 +54,6 @@ const createPayment = async (req, res, next) => {
 };
 
 module.exports = {
-  createPayment,
+  getPaymentsAdminAllById,
+  getPaymentById,
 };
