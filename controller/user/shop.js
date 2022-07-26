481f8084768f07dbe16d6f6f8f8d4e3074fc4fcd
helder-hanka <hfernandes238@gmail.com>
2022-07-26 18:18:54 +0200

    Add getProductByName

--- controller/user/shop.js
+++ controller/user/shop.js
@@ -1,4 +1,5 @@
 const Admin = require("../../model/admin/admin");
+const Products = require("../../model/admin/product");
 
 const getProductAll = async (req, res, next) => {
   try {
@@ -19,6 +20,35 @@ const getProductAll = async (req, res, next) => {
   }
 };
 
+const getProductByName = async (req, res, next) => {
+  const name = req.params.name;
+
+  try {
+    const product = await Products.find({
+      title: new RegExp("^" + name + "$", "i"),
+      confirmDisplay: true,
+      quantity: { $gt: 0 },
+    }).populate({
+      path: "admin",
+      match: { display: true },
+      select: "email",
+      populate: { path: "adress", select: "firstname lastname city country" },
+      populate: { path: "likes", select: "likes" },
+    });
+
+    if (!product.length) {
+      return res.status(404).json({ message: "Could not find product" });
+    }
+    res.status(200).json({ message: "Fetched", product: product });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
 module.exports = {
   getProductAll,
+  getProductByName,
 };
--- routes/userShop.js
+++ routes/userShop.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const shop = require("../controller/user/shop");
 
 router.get("/", shop.getProductAll);
+router.get("/:name", shop.getProductByName);
 
 module.exports = router;
