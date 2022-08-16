b8bed3a2f9cb4a87d3ae2b1feb66fffc2b2e134f
helder-hanka <hfernandes238@gmail.com>
2022-08-16 10:11:51 +0200

    Delete getProductAll and add getProductList with query

--- controller/user/shop.js
+++ controller/user/shop.js
@@ -1,45 +1,103 @@
-const Admin = require("../../model/admin/admin");
 const Products = require("../../model/admin/product");
 
-const getProductAll = async (req, res, next) => {
-  try {
-    const product = await Admin.find({ display: true }, "email")
-      .populate("adress", "firstname lastname city country")
-      .populate("likes")
-      .populate({ path: "Products", match: { quantity: { $gt: 0 } } });
+const getProductList = async (req, res, next) => {
+  const rQuery = req.query;
+  const page = req.query.page;
+  const currentPage = page ? parseInt(page) : 1;
+  const perpage = 20;
 
-    if (!product) {
-      return res.status(404).json({ message: "Could not find product" });
-    }
-    res.status(200).json({ message: "Fetched", product: product });
-  } catch (err) {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
+  let query = [
+    {
+      $sort: { createdAt: -1 },
+    },
+    {
+      $lookup: {
+        from: "admins",
+        localField: "admin",
+        foreignField: "_id",
+        as: "Admin",
+      },
+    },
+    { $unwind: "$Admin" },
+    {
+      $lookup: {
+        from: "adminadresses",
+        localField: "admin",
+        foreignField: "admin",
+        as: "Adress",
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "admin",
+        foreignField: "admin",
+        as: "Like",
+      },
+    },
+    {
+      $match: { "Admin.display": true },
+    },
+
+    {
+      $project: {
+        title: 1,
+        description: 1,
+        like: 1,
+        quantity: 1,
+        price: 1,
+        category: 1,
+        confirmDisplay: 1,
+        imageUrl: 1,
+        admin: 1,
+        Admin: [
+          {
+            _id: "$Admin._id",
+            email: "$Admin.email",
+            display: "$Admin.display",
+          },
+        ],
+        Adress: "$Adress",
+      },
+    },
+    { $unwind: "$Admin" },
+    { $unwind: "$Adress" },
+    { $unset: ["_id"] },
+    // { $limit: 20 },
+  ];
+  const match = [
+    {
+      $match: {
+        confirmDisplay: true,
+        quantity: { $gt: 0 },
+      },
+    },
+    { $skip: (currentPage - 1) * perpage },
+    { $limit: perpage },
+  ];
+
+  if (!rQuery.length) {
+    query.push(...match);
   }
-};
 
-const getProductByName = async (req, res, next) => {
-  const name = req.params.name;
-
+  if (rQuery.title && rQuery.title != "") {
+    match[0].$match.title = new RegExp("^" + rQuery.title + "$", "i");
+    query.push(...match);
+  }
+  if (rQuery.category && rQuery.category != "") {
+    match[0].$match.category = new RegExp("^" + rQuery.category + "$", "i");
+    query.push(...match);
+  }
   try {
-    const product = await Products.find({
-      title: new RegExp("^" + name + "$", "i"),
-      confirmDisplay: true,
-      quantity: { $gt: 0 },
-    }).populate({
-      path: "admin",
-      match: { display: true },
-      select: "email",
-      populate: { path: "adress", select: "firstname lastname city country" },
-      populate: { path: "likes", select: "likes" },
-    });
+    const totalItems = await Products.countDocuments(query);
+    const product = await Products.aggregate(query);
 
     if (!product.length) {
       return res.status(404).json({ message: "Could not find product" });
     }
-    res.status(200).json({ message: "Fetched", product: product });
+    res
+      .status(200)
+      .json({ message: "Fetched", products: product, totalItems: totalItems });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
@@ -49,6 +107,5 @@ const getProductByName = async (req, res, next) => {
 };
 
 module.exports = {
-  getProductAll,
-  getProductByName,
+  getProductList,
 };
--- routes/userShop.js
+++ routes/userShop.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const shop = require("../controller/user/shop");
 
-router.get("/", shop.getProductAll);
-router.get("/:name", shop.getProductByName);
+router.get("/", shop.getProductList);
 
 module.exports = router;
