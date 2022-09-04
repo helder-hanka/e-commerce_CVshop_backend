46c4ccbe72751ee545797bdb9dd171e7c855c94b
helder-hanka <hfernandes238@gmail.com>
2022-09-04 22:37:10 +0200

    create addOrderProgress function, postOrderInProgress

--- controller/user/shop.js
+++ controller/user/shop.js
@@ -1,7 +1,52 @@
-const { validationResult } = require("express-validator");
 const Products = require("../../model/admin/product");
 const Order = require("../../model/user/order");
 const objLength = require("../../lib/object-length");
+const AdressUser = require("../../model/user/adress");
+
+const OrderInProgress = require("../../model/user/orderInProgress");
+const OrderInProgressAdmin = require("../../model/admin/orderInProgress");
+
+function addOrderProgress(product) {
+  const orderItem = product;
+  if (typeof orderItem != "object") {
+    return null;
+  }
+
+  orderItem.forEach((value) => {
+    delete value.Admin;
+  });
+
+  const ListAdminIdOrder = [
+    ...new Set(orderItem.map((value) => value.admin)).values(),
+  ];
+
+  const hashmapAdminOrder = new Map();
+
+  for (let i = 0; i < ListAdminIdOrder.length; i++) {
+    const orders = orderItem.filter(
+      (value) => value.admin == ListAdminIdOrder[i]
+    );
+
+    hashmapAdminOrder.set(ListAdminIdOrder[i], orders);
+  }
+  let newResult = [];
+
+  hashmapAdminOrder.forEach((value, key) => {
+    const reduced = value.reduce(
+      (r, { price, quantity }) => {
+        r["quantityTotal"] += quantity;
+        r["priceTotal"] += price * quantity;
+        return r;
+      },
+      { quantityTotal: 0, priceTotal: 0, admin: key }
+    );
+    reduced["products"] = value;
+
+    newResult.push(reduced);
+  });
+
+  return newResult;
+}
 
 const getProductList = async (req, res, next) => {
   const rQuery = req.query;
@@ -113,26 +158,103 @@ const getProductList = async (req, res, next) => {
   }
 };
 
-const postOrder = async (req, res, next) => {
-  const errors = validationResult(req);
+const postOrderInProgress = async (req, res, next) => {
   const userId = req.userId;
   const products = req.body;
-  if (!errors.isEmpty()) {
-    return res.status(422).json(errors.array());
-  }
+
   if (objLength(products.items) !== 2 || objLength(products.products) <= 0) {
     return res.status(422).json({ message: "Order is empty" });
   }
 
   try {
+    const name = await AdressUser.findOne({ user: userId }).populate(
+      "user",
+      "email"
+    );
+    if (!name) {
+      return res.status(404).json({ message: "Could not find User !" });
+    }
     const resultProd = await products.products.map((i) => {
       return { products: { ...i } };
     });
+    const order = new OrderInProgress({
+      user: {
+        userId: userId,
+        name: name.firstname,
+        email: name.user.email,
+      },
+      userValidation: false,
+      adminValidation: false,
+      quantityTotal: products.items.quantityTotal,
+      priceTotal: products.items.priceTotal,
+      products: resultProd,
+    });
+    const user = {
+      userId: userId,
+      name: name.firstname,
+      email: name.user.email,
+      mobile: name.mobile,
+    };
+
+    const result = addOrderProgress(products.products);
+    if (!result) {
+      return res
+        .status(404)
+        .json({ message: "Current order not registered !" });
+    }
+
+    const postOrder = result.map(async (i) => {
+      const productsSave = [];
+      const a = new OrderInProgressAdmin({
+        priceTotal: i.priceTotal,
+        quantityTotal: i.quantityTotal,
+        products: i.products,
+        user: user,
+        userValidation: i.userValidation || false,
+        adminValidation: i.adminValidation || false,
+      });
+      const result = await a.save();
+      productsSave.push(await result);
+      // console.log("productsSave: ", productsSave);
+      return result;
+    });
+    console.log("productsSave: ", productsSave);
+    console.log("postOrder: ", postOrder);
+    return res
+      .status(200)
+      .json({ message: "Validated orders", order: postOrder });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+const postOrder = async (req, res, next) => {
+  const userId = req.userId;
+  const products = req.body;
+
+  if (objLength(products.items) !== 2 || objLength(products.products) <= 0) {
+    return res.status(422).json({ message: "Order is empty" });
+  }
+
+  try {
+    const name = await AdressUser.findOne({ user: userId }).populate(
+      "user",
+      "email"
+    );
+    if (!name) {
+      return res.status(404).json("Could not find User !");
+    }
+    const resultProd = await products.products.map((i) => {
+      return { products: i };
+    });
     const order = new Order({
       user: {
-        userId: "62dea64ca0d502a1cea6858e",
-        name: "user1",
-        email: "user1@gmail.com",
+        userId: userId,
+        name: name.firstname,
+        email: name.user.email,
       },
       quantityTotal: products.items.quantityTotal,
       priceTotal: products.items.priceTotal,
@@ -151,4 +273,5 @@ const postOrder = async (req, res, next) => {
 module.exports = {
   getProductList,
   postOrder,
+  postOrderInProgress,
 };
